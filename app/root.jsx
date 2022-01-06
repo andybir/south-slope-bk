8258721ec484823b2c3f82bf066a3a00eee8f5bd
Andy Birosak <andyb@Andys-MacBook-Pro.local>
2022-01-06 15:20:15 -0500

    refactor root.jsx

--- app/root.jsx
+++ app/root.jsx
@@ -5,19 +5,35 @@ import {
   Meta,
   Outlet,
   Scripts,
-  ScrollRestoration
-} from "remix";
+  ScrollRestoration,
+} from 'remix'
+import globalstylesUrl from '~/styles/global.css'
 
-export function meta() {
-  return { title: "South Slope BK" };
+export const links = () => [{ rel: 'stylesheet', href: globalstylesUrl }]
+
+export const meta = () => {
+  const description = 'A website for all things South Slope, Brooklyn'
+  const keywords = 'south slope, brooklyn, restaurants, bars'
+  const title = 'South Slope BK'
+  return { description, keywords, title }
 }
 
 export default function App() {
   return (
-    <html lang="en">
+    <Document>
+      <Layout>
+        <Outlet />
+      </Layout>
+    </Document>
+  )
+}
+
+function Document({ children, title }) {
+  return (
+    <html lang='en'>
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width,initial-scale=1' />
         <Meta />
         <Links />
       </head>
@@ -25,8 +41,39 @@ export default function App() {
         <Outlet />
         <ScrollRestoration />
         <Scripts />
-        {process.env.NODE_ENV === "development" && <LiveReload />}
+        {process.env.NODE_ENV === 'development' && <LiveReload />}
       </body>
     </html>
-  );
+  )
+}
+
+function Layout({ children }) {
+  return (
+    <>
+      <nav className='navbar'>
+        <Link to='/' className='logo'>
+          South Slope
+        </Link>
+
+        <ul className='nav'>
+          <li>
+            <Link to=''>Menu Item 1</Link>
+          </li>
+        </ul>
+      </nav>
+      <div className='container'>{children}</div>
+    </>
+  )
+}
+
+export function ErrorBoundary({ error }) {
+  console.log(error)
+  return (
+    <Document>
+      <Layout>
+        <h1>Error</h1>
+        <p>{error.message}</p>
+      </Layout>
+    </Document>
+  )
 }
